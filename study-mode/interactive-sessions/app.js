c8a0e6ffd6c8f5714f5a49d458bb07b89e5f4861
Shalom Bharathi <154990142+Shalom-Bharathi@users.noreply.github.com>
2025-01-19 22:13:33 +0530

    Update app.js

--- study-mode/interactive-sessions/app.js
+++ study-mode/interactive-sessions/app.js
@@ -1,1 +1,11 @@
+let API_KEY;
+let thingsRefx;
+let unsubscribex;
+let db = firebase.firestore();
+thingsRefx = db.collection('API');
 
+unsubscribex = thingsRefx.onSnapshot(querySnapshot => {
+  querySnapshot.docs.forEach(doc => {
+    API_KEY = doc.data().API;
+  });
+});
